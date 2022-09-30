7be234dadbac2ab90219a0c5efd2fbc81a6a7ee1
EricFithian <ef2413@columbia.edu>
2022-09-30 12:30:31 -0400

    Updated with models being added and admin to create artists

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Artist(models.Model):
+
+    name = models.CharField(max_length=150)
+    img = models.CharField(max_length=500)
+    bio = models.TextField(max_length=1000)
+    verified_artist = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
