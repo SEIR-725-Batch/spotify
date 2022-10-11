5972964db5fa63f790868ee41d9fed5043fd0c42
EricFithian <ef2413@columbia.edu>
2022-10-10 22:11:10 -0400

    Added a profile create page

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Artist, Song, Playlist
+from .models import Artist, Song, Playlist, Profile
 
 # Register your models here.
 admin.site.register(Artist)
 admin.site.register(Song)
 admin.site.register(Playlist)
+admin.site.register(Profile)
